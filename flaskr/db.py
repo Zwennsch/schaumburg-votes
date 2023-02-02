2f04409c2330acf4679fff730435b4a577975b57
Sven Schroeder <schuen@gmx.de>
2023-02-02 15:21:06 +0100

    finished db.py

--- flaskr/db.py
+++ flaskr/db.py
@@ -5,4 +5,18 @@ from flask import current_app, g
 
 def get_db():
     if 'db' not in g:
-        g.db = sqlite3.connect(current_app.config['DATABASE'])
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory=sqlite3.Row
+
+    return g.db
+
+def close_db(e=None):
+    # the pop method on the g object gets and removes the given attribute by name
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
