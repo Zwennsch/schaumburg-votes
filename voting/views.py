2282d3ba823f74e8358202166c6b88d83450018f
Sven Schroeder <schuen@gmx.de>
2023-02-23 16:57:11 +0100

    completed vote endpoint

--- voting/views.py
+++ voting/views.py
@@ -10,44 +10,46 @@ bp = Blueprint('views', __name__)
 @login_required
 def vote():
     if request.method == 'GET':
-        # if g.user['vote_passed'] == 1:
-        #     flash("already voted, you can change your vote now")
         return render_template('views/vote2.html')
+
     # case for POST
     else:
-        # TODO: need to update if user has already voted
-
         wahl_1 = request.form.get("wahl1")
         wahl_2 = request.form.get("wahl2")
         wahl_3 = request.form.get("wahl3")
 
         # check that a vote for every choice has been made:
         if None in (wahl_1, wahl_2, wahl_3):
-            flash("Invalid Vote. Please vote again.", "warning")
-            # flashes = session.get(['_flashes'])
+            flash("Invalid vote. Please vote again.", "warning")
             return redirect(url_for('views.vote'))
 
-        vote = g.user['vote_passed']
-        #update if vote already passed
         db = get_db()
-        if g.user['vote_passed'] == 1:
-           # db.execute("")
-           print('vote already passed')
-           pass
-        # save votes into vote-table in db 
         id = g.user['id']
 
-        db.execute(
-            "INSERT INTO vote (user_id, date_created, first_vote, second_vote, third_vote)"
-            "VALUES (?,datetime('now', 'localtime'),?,?,?)",
-            (id, wahl_1, wahl_2, wahl_3)
-        )
-        # update vote_passed for user
-        db.execute(
-            "UPDATE user SET vote_passed = 1 WHERE id = ?",
-            (id,)
-        )
-        flash("your vote has been saved!", "success")
+        #update if vote already passed
+        if g.user['vote_passed'] == 1:
+           db.execute(
+                "UPDATE vote SET first_vote = ?, second_vote = ?, third_vote = ?,"
+                "date_created = datetime('now', 'localtime')"
+                "WHERE user_id = ?", 
+                (wahl_1, wahl_2, wahl_3, id)
+           )
+        #    print('vote already passed')
+           flash("your vote has been updated", "success")
+
+        # save first votes into vote-table in db 
+        else:
+            db.execute(
+                "INSERT INTO vote (user_id, date_created, first_vote, second_vote, third_vote)"
+                "VALUES (?,datetime('now', 'localtime'),?,?,?)",
+                (id, wahl_1, wahl_2, wahl_3)
+            )
+            # update vote_passed for user
+            db.execute(
+                "UPDATE user SET vote_passed = 1 WHERE id = ?",
+                (id,)
+            )
+            flash("Your Vote has been saved!", "success")
         db.commit()
 
     return redirect(url_for('views.index'))
