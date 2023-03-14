6bf15a090e8a1e2187b9044061abe93bbc17c275
Sven Schroeder <schuen@gmx>
2023-03-14 17:00:11 +0100

    working on tests

---
+++ tests/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO user (first_name, last_name, username, password_hash, class)
+VALUES
+    ('test_first_name','test_last_name','test_username','pbkdf2:sha256:260000$M0BmdefyOnOAiRWP$86fa6ba7259d598a5310ccb875c59cd2b3b334f1ca11030330f37815098c45fa','8a'),
+    ('other_first_name','other_last_name','other_username','pbkdf2:sha256:260000$M0BmdefyOnOAiRWP$86fa6ba7259d598a5310ccb875c59cd2b3b334f1ca11030330f37815098c45fa','8a');
